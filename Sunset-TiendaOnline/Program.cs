d0b7187bf689bacb7002cd80c715f593f47c1d65
gomezaldana <aldana0gomez@gmail.com>
2026-01-29 15:36:26 -0300

    Cambio configuracion de dbContext y builder.Services

--- Sunset-TiendaOnline/Program.cs
+++ Sunset-TiendaOnline/Program.cs
@@ -1,8 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using Sunset_TiendaOnline.Data;
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
 DotNetEnv.Env.Load();
 
-builder.Services.AddControllers();
+// para poder usar la bd
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        $"Host={Environment.GetEnvironmentVariable("DB_HOST") ?? "localhost"};" +
+        $"Port={Environment.GetEnvironmentVariable("DB_PORT") ?? "5432"};" +
+        $"Database={Environment.GetEnvironmentVariable("DB_NAME") ?? "sunset"};" +
+        $"Username={Environment.GetEnvironmentVariable("DB_USER") ?? "postgres"};" +
+        $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "postgres"}"
+    )
+);
+
+
+builder.Services
+    .AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.Converters.Add(
+            new JsonStringEnumConverter()
+        );
+    });
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
